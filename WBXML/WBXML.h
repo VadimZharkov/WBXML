25c64f9a68c77c6d9675ad8584ac44e4cbc865c3
Vadim Zharkov <vadim.zharkov@gmail.com>
2020-05-25 10:05:19 +0300

    Initial Commit

---
+++ WBXML/WBXML.h
@@ -0,0 +1,19 @@
+//
+//  WBXML.h
+//  WBXML
+//
+//  Created by Vadim Zharkov on 25.05.2020.
+//  Copyright © 2020 Vadim Zharkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WBXML.
+FOUNDATION_EXPORT double WBXMLVersionNumber;
+
+//! Project version string for WBXML.
+FOUNDATION_EXPORT const unsigned char WBXMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WBXML/PublicHeader.h>
+
+
